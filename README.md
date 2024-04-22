a05d8344d3e02dfaf57af64e2ab102f3605f78dc
m.hosseini <131990198+mahan-hosseini@users.noreply.github.com>
2024-04-22 10:26:54 +0200

    Not pinning dependencies (instead saying at least version xyz)

--- README.md
+++ README.md
@@ -22,7 +22,7 @@ It is strongly recommended that a separate virtual environment for AutoGaitA is 
     - *Windows:* `env_gaita\Scripts\activate`
     - *Mac:* `source env_gaita/bin/activate`
 
-- Once activated, install AutoGaitA in the virtual environment via pip: `pip install autogaita`
+- Once activated, install AutoGaitA in the virtual environment via pip: `pip install autogaita`.
 
 - Access The main user interface via `python -m autogaita`.
 
--- setup.py
+++ setup.py
@@ -3,17 +3,17 @@ from setuptools import setup
 import platform
 
 # list of across-platform dependencies
-install_requires=[
-    'customtkinter==5.2.0',
-    'pandas==2.0.3',
-    'numpy==1.24.2',
-    'matplotlib==3.7.1',
-    'scikit-learn==1.2.2',
-    'pingouin==0.5.3',
-    'scipy==1.11.1',
-    'ffmpeg-python==0.2.0',
-    'openpyxl==3.1.2',
-    'pillow==10.3.0'
+install_requires = [
+    "customtkinter>=5.2",
+    "pandas>=2.0",
+    "numpy>=1.24",
+    "matplotlib>=3.7",
+    "scikit-learn>=1.2",
+    "pingouin>=0.5",
+    "scipy>=1.11",
+    "ffmpeg-python>=0.2",
+    "openpyxl>=3.1",
+    "pillow>=10.3",
 ]
 
 # add platform-specific dependencies
@@ -22,13 +22,13 @@ if platform.system() == "Darwin":
 
 # call setup function
 setup(
-    name='autogaita',
-    version='0.0.5',
+    name="autogaita",
+    version="0.0.5",
     author="Mahan Hosseini",
     description="Automatic Gait Analysis in Python",
     packages=["autogaita", "autogaita.batchrun_scripts"],
     include_package_data=True,
-    package_data={'': ['*.txt', '*.rst', '*.png', "*.icns", "*.ico"]},
+    package_data={"": ["*.txt", "*.rst", "*.png", "*.icns", "*.ico"]},
     install_requires=install_requires,
     license="GPLv3",
     url="https://github.com/mahan-hosseini/AutoGaitA/",
