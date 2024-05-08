29889ded3da9f6df88a32d3dc5df52fab9f74f56
m.hosseini <131990198+mahan-hosseini@users.noreply.github.com>
2024-05-08 18:09:11 +0200

    Add dlc unit tests of velocity and non-beam height normalisation

--- tests/test_dlc_unit.py
+++ tests/test_dlc_unit.py
@@ -1,6 +1,7 @@
-from autogaita.autogaita_dlc import compute_angle, add_angles
+from autogaita.autogaita_dlc import compute_angle, add_angles, add_velocities
 import numpy as np
 import pandas as pd
+import pandas.testing as pdt
 import pytest
 import pdb
 
@@ -17,15 +18,33 @@ import pdb
 # %%.......................  step-cycle extraction  ................................
 
 # %%.......  main analysis: sc-lvl y-norm, features, df-creation & export ..........
-# test_angles()
 
-# %%..............................  plots  .........................................
 
-# %%..........................  print finish  ......................................
+def test_dlc_height_normalisation_no_beam():
+    """Unit test normalising heights if there is no beam"""
+    # Create a sample DataFrame
+    data = {
+        "first_y": [-10, -5, 0, 10],
+        "second_y": [10, 20, 30, 40],
+        "third_y": [5, 15, 25, 35],
+    }
+    step = pd.DataFrame(data)
+    y_cols = [col for col in step.columns if col.endswith("y")]
+    this_y_min = step[y_cols].min().min()
+    step[y_cols] -= this_y_min
+    # Expected result
+    expected_result = {
+        "first_y": [0, 5, 10, 20],
+        "second_y": [20, 30, 40, 50],
+        "third_y": [15, 25, 35, 45],
+    }
+    expected_step = pd.DataFrame(expected_result)
+    # Compare the result with the expected result
+    pdt.assert_frame_equal(step, expected_step)
 
 
 def test_dlc_angles():
-    """Unit test of compute_angles local function of dlc"""
+    """Unit test of how angles are added"""
     # note that np.array line initalises step's joint-coords according to:
     # joint_angle = (0, 0)
     # joint2 = (1, 0)
@@ -58,6 +77,46 @@ def test_dlc_angles():
     # assert compute_angle(joint_angle, joint2, joint3) == expected_angle
 
 
+def test_dlc_velocities():
+    """Unit test of how velocities are added"""
+    data = {
+        "Sample x": [0.0, 2.0, 4.0, 8.0, 8.0, 4.0, 2.0, 0.0, -2.0, -10.0],
+        "Sample Angle": [-10.0, -8.0, -7.0, -4.0, 0.0, 4.0, 10.0, 20.0, 10.0, 0.0],
+    }
+    step = pd.DataFrame(data)
+    cfg = {
+        "hind_joints": ["Sample "],
+        "x_acceleration": True,
+        "angular_acceleration": True,
+    }
+    step = add_velocities(step, cfg)
+    # expected values were obtained by calling np.gradient on arrays above
+    expected_values = {
+        "Sample Velocity": [2.0, 2.0, 3.0, 2.0, -2.0, -3.0, -2.0, -2.0, -5.0, -8.0],
+        "Sample Acceleration": [0.0, 0.5, 0.0, -2.5, -2.5, 0.0, 0.5, -1.5, -3.0, -3.0],
+        "Sample Angle Velocity": [2.0, 1.5, 2.0, 3.5, 4.0, 5.0, 8.0, 0.0, -10.0, -10.0],
+        "Sample Angle Acceleration": [
+            -0.5,
+            0.0,
+            1.0,
+            1.0,
+            0.75,
+            2.0,
+            -2.5,
+            -9.0,
+            -5.0,
+            0.0,
+        ],
+    }
+    for key in expected_values.keys():
+        assert all(expected_values[key] == step[key])
+
+
+# %%..............................  plots  .........................................
+
+# %%..........................  print finish  ......................................
+
+
 # what happens if we hit run
 if __name__ == "__main__":
-    test_dlc_angles()
+    test_dlc_velocities()
