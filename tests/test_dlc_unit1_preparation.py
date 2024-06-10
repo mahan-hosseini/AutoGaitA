43fd7c3fa57342555aa7a1d4cb63e3cf6f9baf63
m.hosseini <131990198+mahan-hosseini@users.noreply.github.com>
2024-06-10 17:40:22 +0200

    unittesttt

--- tests/test_dlc_unit1_preparation.py
+++ tests/test_dlc_unit1_preparation.py
@@ -170,14 +170,11 @@ def test_removal_of_wrong_strings_from_cfg_key(test_list, extract_data_using_som
 def test_flip_mouse_body(extract_data_using_some_prep, extract_info):
     data = extract_data_using_some_prep
     flipped_data = extract_data_using_some_prep
-    test_data = data.copy()
-    # data = flip_mouse_body(extract_data_using_some_prep, extract_info)
+    flipped_data = flip_mouse_body(flipped_data, extract_info)
     # flipped_data = data.copy()
     # test_data = data.copy()
     print("data:")
     print(data)
-    print("test_data:")
-    print(test_data)
     print("flipped_data:")
     print(flipped_data)
     for col in flipped_data.columns:
