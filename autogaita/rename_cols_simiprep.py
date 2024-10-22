957c73e77815ad179fe05e73a263ff646d6b76b0
m.hosseini <131990198+mahan-hosseini@users.noreply.github.com>
2024-10-22 17:07:31 +0200

    First rough workflow. NUs in comments

--- autogaita/rename_cols_simiprep.py
+++ autogaita/rename_cols_simiprep.py
@@ -1,74 +1,85 @@
 import pandas as pd
+import pdb
+
+CANDIDATE_SIDE_IDENTIFIER = ["l", "r", "left", "right"]  # user-string forced lowercase
+CANDIDATE_COORD_IDENTIFIER = ["x", "y", "z"]
+SIDE_KEY = "side"
+COORD_KEY = "coordinate"
+JOINT_KEY = "joint"
+COMMA_SEP = ", "
+SPACE_SEP = " "
+
+# NU
+# => could add an option to either remove or keep the cols that are not renamed
+# => save to xls!
+# => tidy it up and check if the generated xls works with gaita_simi for the renamed cols
+# => if my first manual tests seem to work, then write proper unit & property tests for this
+# => finally add to GUI
 
 
+def main():
+    rename_master(
+        "/Users/mahan/sciebo/Research/AutoGaitA/Human/Sebastian/Sebastians_Data_combined_xyz.csv",
+        "_",
+    )
 
 
-### IMPORTANT NOTE ###
-# Make sure to account for cols that do not have a side (e.g. Pelvis X) etc.
+def rename_master(root_dir, separator):
+    """Note - separator must be unique in string, e.g. cannot be in key point name"""
+    original_df = pd.read_csv(root_dir)
+    # replace unwanted strings
+    df = replace_strings_in_colnames(original_df, "_xyz")
+    # loop over all cols in df, rename func renames cols only if needed
+    # => and if so, based on side-specificity or not
+    new_col_strings = []
+    for col_string in df.columns:
+        new_col_strings.append(rename_a_column(col_string, separator))
+    # rename the cols in the df
+    df.columns = new_col_strings
+    # store df
+    pdb.set_trace()
+    df.to_csv(
+        "/Users/mahan/sciebo/Research/AutoGaitA/Human/Sebastian/Sebastians_Data_combined_xyz_renamed.csv"
+    )
 
 
-
-
-"""
-Input is 3 identifiers and 2 separators which together make the full string of the original column name. Use these to rename all columns containing 3D coordinates to the naming convention required by AutoGaitA Universal 3. For that:
-1. (Optional) - remove cols that have unnecessary identifier
-2. Extract and store the three identifiers based on the separators.
-3. Rename the columns to the new naming convention using the three identifiers
-"""
-
-# ONE COL NAME AT A TIME FOR RENAMING
-# THEN GIVE NEW COL NAME TO DF
-def rename_columns(root_dir, id1, id2, id3, sep1, sep2):
-    new_columns = {}
-    try:
-        df = pd.load_excel(root_dir)
-    except:
-        # df = pd.read_csv(root_dir + "/Users/mahan/sciebo/Research/AutoGaitA/Human/Sebastian/Sebastians_Data_combined_xyz.csv")
-        df = pd.read_csv(
-            "/Users/mahan/sciebo/Research/AutoGaitA/Human/Sebastian/Sebastians_Data_combined_xyz.csv"
-        )
-    id_1 = "side"
-    id_2 = "key_point"
-    id_3 = "coordinate"
-    separator = "_"
-
-
-def check_if_col_needs_renaming(col_string, separator):
-    if col_string.count(separator) == 2:
-        return True
-    return False
-
-from typing import Dict
-def extract_user_identifiers(col_string:str, separator:str): -> Dict[str, str]:
-    # input and output types with : syntax und Dict[str, str]
-    candidate_side_identifier = ["l", "r", "left", "right"]
-    candidate_coord_identifier = ["x", "y", "z"]
-    id_dict = {}
+def rename_a_column(col_string, separator):
+    """
+    Renames a column string based on specific identifiers separated by a given separator.
+    1) Extracts the identifiers based on the separators (check capitsalisation comments)
+    2) Returns the new column name based on whether this column is side-specific, central, or not a coordinate column at all
+    """
+    id_dict = {}  # just used locally in this func to get (new) col names
+    # first, extract identifiers based on separators
+    # => if statement with .lower() to make our life easy
+    # => note in coord_key the OUTPUT (!) should be capitalised!
     for candidate_id in col_string.split(separator):
-        if candidate_id.lower() in candidate_side_identifier:
-            id_dict["side"] = candidate_id
-        elif candidate_id.lower() in candidate_coord_identifier:
-            id_dict["coordinate"] = candidate_id
+        if candidate_id.lower() in CANDIDATE_SIDE_IDENTIFIER:
+            if candidate_id in ["l", "left"]:
+                id_dict[SIDE_KEY] = "left"
+            if candidate_id in ["r", "right"]:
+                id_dict[SIDE_KEY] = "right"
+        elif candidate_id.lower() in CANDIDATE_COORD_IDENTIFIER:
+            id_dict[COORD_KEY] = candidate_id.capitalize()  # capitalise!
         else:
-            id_dict["key_point"] = candidate_id
-    
-    new_col_string = f"{id_dict["key_point"]}, {id_dict["side"]} {id_dict["coordinate"]}"
+            id_dict[JOINT_KEY] = candidate_id
+    # then, return the new col name based on whether this col is side-specific,
+    # central, or not a coordinate column at all
+    if id_dict.keys() == {COORD_KEY, JOINT_KEY}:
+        return f"{id_dict[JOINT_KEY]}{SPACE_SEP}{id_dict[COORD_KEY]}"
+    elif id_dict.keys() == {SIDE_KEY, COORD_KEY, JOINT_KEY}:
+        return f"{id_dict[JOINT_KEY]}{COMMA_SEP}{id_dict[SIDE_KEY]}{SPACE_SEP}{id_dict[COORD_KEY]}"
+    else:
+        return col_string
 
-extract_user_identifiers()
-def rename_one_column_string(old_string)
-
-
-first make it lowercase for l/r left/right before comparing to list of valid names
-valid coords
-separators should be "unique", just start with underscore, in any case seperators cannot be in key point name
-
-also use " if side identifier " in col_string to check whether we have to use rename_side_specific_col or rename_central_col (e.g. Pelvis)
 
 # NU - add property test for this
 # This function is done. Have it be a separate function that users can use to clean their cols before running the main function (so if there should be 3 parts that are unnecessary, people call this function 3 times before running the main function)
 # => make this be a separate button in GUI
 df = pd.DataFrame(data=None, columns=["a", "a_xyz"])
 replace_string = "_xyz"
+
+
 def replace_strings_in_colnames(df, replace_string):
     """
     Function to remove unnecessary sub-strings in colnames prior to  batch renaming
@@ -80,3 +91,8 @@ def replace_strings_in_colnames(df, replace_string):
             new_col = col.replace(replace_string, "")
             new_col_dict[col] = new_col
     df.rename(columns=new_col_dict, inplace=True)
+    return df
+
+
+if __name__ == "__main__":
+    main()
